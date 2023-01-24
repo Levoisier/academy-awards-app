f0d51e28e2448c99cd36cca453c36a91fe57a417
Cristian Zapata <c.zapata.iq@gmail.com>
2023-01-24 14:02:55 -0500

    > Poll views

--- academyawardsapp/academyawardsapp/settings.py
+++ academyawardsapp/academyawardsapp/settings.py
@@ -12,14 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-2qprl9xgmo4a5$9#rf1-b@k(i+$flbf1u%3tz_029prvh1h^4k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
